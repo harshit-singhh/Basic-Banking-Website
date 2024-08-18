fe0f76e22b5de2ff6fe2e59afbd0e1fb195765b2
Harshit Singh <harshitsingh81203@gmail.com>
2024-08-19 00:07:44 +0530

    Update user-list.component.js

--- frontend/src/components/user-list.component.js
+++ frontend/src/components/user-list.component.js
@@ -8,13 +8,8 @@ const userList =()=>{
     const [user, setUser] = useState([]);
 
     useEffect(()=>{
-<<<<<<< HEAD
-        axios
-          .get(`${process.env.BACKEND_URL}users/`)
-          .then(({ data }) => {
-=======
+
         axios.get("https://basic-banking-website-wbll.onrender.com/users/").then(({data})=>{
->>>>>>> f8c7e145de8067818acf66979ba47b99f94bd23e
             setUser(data);
           })
           .catch((err) => {
